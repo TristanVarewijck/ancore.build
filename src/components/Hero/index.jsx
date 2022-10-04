b532a61b603fd1a934982c5ab6c2df339ecbf62b
TristanVarewijck <tristanconnect4@outlook.com>
2022-10-04 16:09:59 +0200

    mobile repsonsive

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,29 +1,44 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+
 import Button from "react-bootstrap/button";
 
 function Hero() {
   return (
-    <Container fluid>
-      <Row className="gx-5">
-        <Col sm={7}>
-          <h1>WE BUILD EVERYTHING DIGITAL</h1>
-          <Card style={{ width: "auto" }}>
-            <Card.Img
-              variant="center"
-              src="https://images.unsplash.com/photo-1606829192980-9a547ffc918c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=800&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2NDg3Nzg3NA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1900"
-            />
-          </Card>
+    <Container fluid="xxl" className="mt-5">
+      <Row className="gx-5 justify-content-center align-items-top">
+        <Col lg={7}>
+          <h1 className="mb-3">WE BUILD EVERYTHING DIGITAL</h1>
+
+          <img
+            src="/assets/images/banner-img.png"
+            class="img-fluid"
+            alt="Responsive image"
+          />
         </Col>
-        <Col sm={5}>
+        <Col lg={5}>
           <p>
             Donec pretium, nisl sed tempor faucibus, tortor sapien sagittis
             felis, bibendum velit est ac nisl. Proin placerat massa sit amet
             ligula
           </p>
           <Button>Get Started</Button>
+
+          <div className="info-1">
+            <h5>LOREM</h5>
+            <p>Donec pretium, nisl sed tempor faucibus, tortor sapien</p>
+          </div>
+
+          <div className="info-1">
+            <h5>LOREM</h5>
+            <p>Donec pretium, nisl sed tempor faucibus, tortor sapien</p>
+          </div>
+
+          <div className="info-1">
+            <h5>LOREM</h5>
+            <p>Donec pretium, nisl sed tempor faucibus, tortor sapien</p>
+          </div>
         </Col>
       </Row>
     </Container>
