4b6c8017e6b961555673122a14eb8374e42e61c6
TristanVarewijck <tristanconnect4@outlook.com>
2022-10-04 11:52:20 +0200

    deployed

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,12 +1,17 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 function Hero() {
   return (
     <Container>
       <Row>
-        <Col sm={8}>sm=8</Col>
+        <Col sm={8}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="center" src="holder.js/100px180" />
+          </Card>
+        </Col>
         <Col sm={4}>sm=4</Col>
       </Row>
     </Container>
